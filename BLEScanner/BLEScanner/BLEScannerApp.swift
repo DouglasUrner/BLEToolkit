7f569bb7b24a2ebaef9346369731f9402f9f1374
Douglas Urner <dlu@canishe.com>
2024-03-23 13:34:07 -0700

    Initial commit.

---
+++ BLEScanner/BLEScanner/BLEScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEScannerApp.swift
+//  BLEScanner
+//
+//  Created by Douglas Urner on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BLEScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
